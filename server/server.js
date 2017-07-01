947599fcb912231a51d5083125f2871467e055bf
Mike Wight <michel.wight@hotmail.com>
2017-07-01 10:16:42 -0300

    add DELETE todos

--- server/server.js
+++ server/server.js
@@ -48,6 +48,21 @@ app.get('/todos/:id', (req, res) => {
     }).catch((e) => res.status(400).send());
 });
 
+app.delete('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send("Not the right id structure");
+    }
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo){
+            return res.status(404).send("Not extit");
+        }
+        res.send(todo);
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
 app.listen(port, () => {
    console.log(`Started on port ${port}`);
 });
