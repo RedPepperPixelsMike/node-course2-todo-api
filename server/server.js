b3634f50bab81cf74c3790ca31561b979e750c8c
Mike Wight <michel.wight@hotmail.com>
2017-07-01 11:02:13 -0300

    Add PAtch /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
-const {ObjectID} =require('mongodb');
-
+const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 
 let express = require('express');
@@ -36,11 +36,11 @@ app.get('/todos', (req, res) => {
 
 app.get('/todos/:id', (req, res) => {
     let id = req.params.id;
-    if(!ObjectID.isValid(id)) {
+    if (!ObjectID.isValid(id)) {
         return res.status(404).send("Not the right id structure");
     }
     Todo.findById(id).then((todo) => {
-        if (!todo){
+        if (!todo) {
             return res.status(404).send("Not the right id number");
         }
 
@@ -50,11 +50,11 @@ app.get('/todos/:id', (req, res) => {
 
 app.delete('/todos/:id', (req, res) => {
     let id = req.params.id;
-    if(!ObjectID.isValid(id)) {
+    if (!ObjectID.isValid(id)) {
         return res.status(404).send("Not the right id structure");
     }
     Todo.findByIdAndRemove(id).then((todo) => {
-        if (!todo){
+        if (!todo) {
             return res.status(404).send("Not extit");
         }
         res.send({todo});
@@ -63,11 +63,39 @@ app.delete('/todos/:id', (req, res) => {
     });
 });
 
+app.patch('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send("Not the right id structure");
+    }
+
+    if (_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id,
+        {$set: body},
+        {new: true}
+    ).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
 app.listen(port, () => {
-   console.log(`Started on port ${port}`);
+    console.log(`Started on port ${port}`);
 });
 
 
 module.exports = {
-  app
+    app
 };
