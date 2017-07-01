7cc3cc06af25bfaaa3d87204c1fbac737fee3eb6
Mike Wight <michel.wight@hotmail.com>
2017-07-01 19:23:08 -0300

    Add POST /users/login rout

--- server/server.js
+++ server/server.js
@@ -109,7 +109,22 @@ app.post('/users', (req, res) => {
 });
 
 app.get('/users/me', authenticate, (req, res) => {
-   res.send(req.user);
+    res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+    let body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password)
+        .then((user) => {
+            return user.generateAuthTokens()
+                .then((token) => {
+                    res.header('x-auth', token).send(user);
+                })
+        })
+        .catch((e) => {
+            res.status(400).send();
+        });
 });
 
 app.listen(port, () => {
